e763ac8e78fdac4b9647012610f95756c520f7f7
thomasthomsen16 <35918236+thomasthomsen16@users.noreply.github.com>
2024-09-10 11:04:22 +0200

    Assign numbers to firstNum and secondNum from numData[]

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let firstNum, secondNum, operator, result;
+let firstNum, secondNum, operator, displayVal;
 
 let numData = [];
 let operatorData = [];
@@ -19,15 +19,12 @@ function divide() {
 
 }
 
-//To do: Populate array with the numbers from input and array with operator -> Have to check if input is number or operator. 
-
 //To do: Set firstNum and secondNum to user input. firstNum can be all equal numbers in array (num%2=0) and secondNum can be all uneven numbers in array (num%2=1)
 
 // display numbers from user input and result from calculation
 function display() {
     const displayCalc = document.querySelector(".result");
     
-
     // Get the input of a button pressed to display in the calculator
     const displayKey = document.querySelectorAll(".calculator button");
     displayKey.forEach(button =>{
@@ -35,11 +32,22 @@ function display() {
             displayCalc.textContent = button.textContent;
             // Checks if button is number or operator and push it to corresponding array
             if (!isNaN(button.textContent)) {
+                // Convert button.textContent to number and add to numData array
                 numData.push(button.textContent*1);
-                console.log(numData);
+
+                // Assign values to firstNum and secondNum based on index % 2
+                for (let i = 0; i < numData.length; i++) {
+                    if (i%2 === 0) {
+                        firstNum = numData[i]
+                        console.log(`firstNum assigned: ${firstNum}`);
+                    } else {
+                        secondNum=numData[i]
+                        console.log(`secondNum assigned: ${secondNum}`);
+                    }
+                }
+
             } else {
                 operatorData.push(button.textContent);
-                console.log(operatorData);
             }
         })
     })
